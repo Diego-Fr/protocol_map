699796eb606292f98debda8c3e1021bb62a85ed8
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2025-10-09 16:52:20 -0300

    proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+    basePath: '/protocolo',
+    assetPrefix: '/protocolo/',
+};
 
 export default nextConfig;
