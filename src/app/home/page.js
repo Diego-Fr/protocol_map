8ff9514d64b136e2f6fcf1e69f47616debff24d3
Diego Freitas <diego.freitas.professional@outlook.com>
2025-10-24 00:56:23 -0300

    text center no mobile

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -15,7 +15,7 @@ export default function Page(){
                     <h5 className="text-3xl font-bold  text-center mb-5">
                         O QUE É O PROTOCOLO? 
                     </h5>
-                    <p className="text-lg  text-center">
+                    <p className="text-lg text-center sm:text-justify">
                         O Protocolo de Escassez Hídrica é um instrumento regulatório estratégico desenvolvido pela SP-ÁGUAS para aprimorar o monitoramento das condições hidrológicas, o mapeamento de riscos e a implementação de medidas de contingência em situações críticas de disponibilidade hídrica no Estado de São Paulo. 
 
                         Esse instrumento de gestão inovador, inspirado em experiências internacionais, é baseado no monitoramento contínuo e alertas precoces, avaliação das vulnerabilidades das bacias hidrográficas e nas implementações de medidas de contingências. Seu objetivo principal é garantir água para todos os usuários, aumentar a segurança e resiliência hídrica do estado de São Paulo. 
@@ -33,7 +33,7 @@ export default function Page(){
                     <h2 className="text-3xl font-bold mb-4 text-center">
                         INDICADORES  
                     </h2>
-                    <p className="text-lg text-justify">
+                    <p className="text-lg text-center sm:text-justify">
                         A identificação e o acompanhamento da seca por meio de seus indicadores hidrometeorológicos como Índice de Precipitação Padronizado
                         (SPI), Índice de Precipitação e Evapotranspiração Padronizado (SPEI), Monitor de Secas da Agência
                         Nacional de Águas e Saneamento Básico (ANA), Índice da Diferença de Vegetação Normalizado
