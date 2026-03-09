fa792862e2d205937bde921f4e89d34653a6cbe2
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2026-03-09 11:51:22 -0300

    atulização mensal

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -148,7 +148,7 @@ const Map = () =>{
 
     useEffect(_=>{
         if(!firstLoad){
-            dispatch(setDate({year: 2026, month: 1}))
+            dispatch(setDate({year: 2026, month: 2}))
             setFirstLoad(true)
             fetchSubugrhis().then(data=>{
                 dispatch(setSubugrhis(data))
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -24,13 +24,13 @@ const Slider = () =>{
 
     const radius = 15;
     const buffer = 15;
-    const months = 5
+    const months = 6
 
     const isMobile = useIsMobile()
 
     const [sliderOptions, setSliderOptions] = useState({
         selectedYear: 2026,
-        selectedMonth: 1
+        selectedMonth: 2
     })
     
 
